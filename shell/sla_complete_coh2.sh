04a1306df8e665b50cab259aa79a1572f733158f
Ashmita <ashmitau@uark.edu>
2026-01-19 14:41:18 -0500

    sla

--- shell/sla_complete_coh2.sh
+++ shell/sla_complete_coh2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=sla_coh2_complete 
-#SBATCH --output=logs/sla_coh2_complete.txt
-#SBATCH --partition=comp72
+#SBATCH --job-name=sla_coh2_complete
+#SBATCH --partition=condo
+#SBATCH --account=samuelbf
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --qos comp
-#SBATCH --tasks-per-node=32
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128G
 #SBATCH --time=72:00:00
+#SBATCH --output=logs/sla_coh2_%j.out
+#SBATCH --error=logs/sla_coh2_%j.err
 
 
 ## configs
@@ -16,5 +18,5 @@ module load gcc/9.3.1 mkl/19.0.5 R/4.2.2 vcftools/0.1.15 plink/5.2
 for i in {0..49}; do
  start_=$((($i * 43) + 350))
  end_=$((($i + 1) * 43 + 350))
- Rscript coh2_block.R sla $start_ $end_ &
+ Rscript scripts/coh2_scripts/coh2_block.R sla $start_ $end_ &
 done
