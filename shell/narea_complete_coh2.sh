7833431668daf0d84c43d60e4eab7f3fd9970af9
Ashmita <ashmitau@uark.edu>
2026-01-19 11:34:37 -0500

    added narea complete model bash cript for coh2 calc

---
+++ shell/narea_complete_coh2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=narea_coh2_complete 
+#SBATCH --output=logs/narea_coh2_complete.txt
+#SBATCH --partition=comp72
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --qos comp
+#SBATCH --tasks-per-node=32
+#SBATCH --time=72:00:00
+
+
+## configs
+module purge
+module load gcc/9.3.1 mkl/19.0.5 R/4.2.2 vcftools/0.1.15 plink/5.2
+
+for i in {0..49}; do
+ start_=$((($i * 43) + 350))
+ end_=$((($i + 1) * 43 + 350))
+ Rscript coh2_block.R narea $start_ $end_ &
+done
