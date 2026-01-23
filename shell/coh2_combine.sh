5d82e2a2f3098bfe519259aeecb4065980a6bc13
Ashmita <ashmitau@uark.edu>
2026-01-22 22:09:37 -0500

    added shell

--- shell/coh2_combine.sh
+++ shell/coh2_combine.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=combine_all coh2 
-#SBATCH --partition=tres72
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --tasks-per-node=32
-#SBATCH --time=1:00:00
-#SBATCH --output=logs/combine_coh2_%j.out
-#SBATCH --error=logs/combine_coh2_%j.err
+#SBATCH --job-name=coh2_combine
+#SBATCH --output=logs/coh2_combine.out
+#SBATCH --error=logs/coh2_combine.err
+#SBATCH --partition=comp06
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6 
+#SBATCH --time=01:00:00
 
 set -euo pipefail
 
