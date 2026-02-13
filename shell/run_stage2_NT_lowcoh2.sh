603da08dd2dda5e7ef506c72d9e5e6697d73e1e5
Ashmita-123 <ashmitabhattarai22@gmail.com>
2026-02-12 22:41:59 -0600

    modified NT ST and PnT and psT bashs cripts

--- shell/run_stage2_NT_lowcoh2.sh
+++ shell/run_stage2_NT_lowcoh2.sh
@@ -15,7 +15,7 @@ module purge
 module load gcc/9.3.1 mkl/19.0.5 R/4.2.2
 
 # ---- your single wave ratio column ----
-wave_col="wave_1812_wave_1574"   # change if your column name is different
+wave_col="wave_1812_wave_1574"   # change if your column name is different for different target trait (stays same for EFMW and MWEF for same target trait)
 
 echo "Running on $(hostname)"
 echo "SLURM_JOB_ID=${SLURM_JOB_ID}"
@@ -23,6 +23,6 @@ echo "wave_col=${wave_col}"
 module list
 
 # This R script runs BOTH CV1 and CV2 and writes:
-# NT_CV1.csv, acNT_CV1.csv, NT_CV2.csv, acNT_CV2.csv
-Rscript scripts/lowest_coh2/g_NT_mwef_lowcoh2.R \
+# NT_CV1.csv, acNT_CV1.csv, NT_CV2.csv, acNT_CV2.csv and change your path to the Rscript as you change the target trait
+Rscript scripts/g_lowest_coh2/g_2_NT_mwef_lowcoh2.R \
   --wave_col "${wave_col}"
